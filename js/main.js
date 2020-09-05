a24ca97cbab57ffe4ba05edc9f77660e5952bad4
David <drea1785@gmail.com>
2020-09-05 16:20:28 -0400

    Update CV

--- js/main.js
+++ js/main.js
@@ -41,10 +41,10 @@ $(document).ready(function() {
         $("#read-more").fadeToggle("fast");
 
         // Change the button
-        if ($("#badge-more").text() == "more") {
-           $("#badge-more").text("less");
+        if ($("#badge-more").text() == "hide") {
+           $("#badge-more").text("Papers Available Upon Request");
         } else {
-           $("#badge-more").text("more");
+           $("#badge-more").text("hide");
         }
 
      });
